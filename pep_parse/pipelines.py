c4afb9f596c0918192e9fd830769d87655a781a3
Стас Затушевский <STAs20160@yandex.ru>
2023-02-25 20:28:32 +0200

    1

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,22 @@
+from .settings import BASE_DIR, FILE_NAME, RESULTS
+
+
+class PepParsePipeline:
+
+    def open_spider(self, spider):
+        self.pep_statuses = {}
+
+    def process_item(self, item, spider):
+        if item['status'] in self.pep_statuses:
+            self.pep_statuses[item['status']] += 1
+        else:
+            self.pep_statuses[item['status']] = 0
+        return item
+
+    def close_spider(self, spider):                   
+        with open(BASE_DIR / RESULTS / FILE_NAME, mode='w', encoding='utf-8') as file:
+            file.write('Статус,Количество\n')
+            for status in self.pep_statuses.keys():
+                file.write(f'{status},{self.pep_statuses[status]}\n')
+            total = sum(self.pep_statuses.values())
+            file.write(f'Total,{total}\n')
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,30 @@
+import scrapy
+import re
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        href = []
+        for tr in response.css('a.pep::attr(href)').getall():
+            if tr not in href:
+                href.append(tr)
+        for link in href[1:-2]:
+            result = 'https://peps.python.org/' + link
+            yield response.follow(result, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        words = response.css('h1.page-title::text').get()
+        word = words.split(' – ')
+
+        data = {
+                'number': re.findall(r'\d+', word[0]),
+                'name': word[-1],
+                'status': response.css('dt:contains("Status") + dd').css('abbr::text').get()
+            }
+        yield PepParseItem(data)
